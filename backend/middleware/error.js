4455218182f0e5a2727945bf64ceda884c59e65a
Bishal Regmi <regmibishal42@gmail.com>
2021-11-17 14:34:43 +0545

    Error handeling in backend

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ const ErrorHandler = require('../utils/errorHandler');
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode ||500;
     err.message = err.message || 'Internal Server Error';
+
+    // Wrong Mongodb Id Error
+    if(err.name === "CastError"){
+        const message = `Resource not found.Invalid Id ${err.path}`;
+        err = new ErrorHandler(message,400);
+    }
      
     res.status(err.statusCode).json({
         success:false,
